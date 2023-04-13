86100aff53b3c775d9715547660cba5cd6b166a0
Tushar Singh <ts360523@gmail.com>
2023-04-13 23:24:27 +0530

    Initial commit - Topic variables

---
+++ index.js
@@ -0,0 +1,10 @@
+//var testvariable = '1234;'
+
+// testvariable = '1234;'
+// console.log('variable - ', testvariable);
+// var testvariable;
+
+
+const variable = '1234'
+variable = '567'
+console.log('let variable - ', variable);
