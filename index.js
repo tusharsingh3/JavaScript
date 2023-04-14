865803633d7934727249e34794ff4937ab4a97a5
Tushar Singh <ts360523@gmail.com>
2023-04-14 23:38:46 +0530

    Added notes.md file for notes

--- index.js
+++ index.js
@@ -1,10 +1,111 @@
-//var testvariable = '1234;'
+// /* Variables In Javascript
+// Variables are containers that store values.
+// Types of variables in javascript:-
+// 1. var (was used before 2015)
+// 2. let (use after 2015)
+// 3. const (use after 2015)
+// */
 
-// testvariable = '1234;'
-// console.log('variable - ', testvariable);
-// var testvariable;
+
+// /*--------------------VAR--------------------*/
+// /*-----First Example-----*/
+// testVariable
+// var testVariable = 10
+// console.log('VARIABLE - ', testVariable)
+
+// /*-----Second Example-----*/
+// testVariable = 10
+// var testVariable
+// console.log('VARIABLE - ', testVariable)
+
+// /*-----Third Example-----*/
+// testVariable = 10
+// console.log('VARIABLE - ', testVariable)
+// var testVariable
+
+// /*-----Fourth Example-----*/
+// testVariable = 10
+// {
+//     testVariable = 20
+// }
+// console.log('VARIABLE - ', testVariable)
+// var testVariable
+
+// /*-----Fifth Example-----*/
+// var testVariable = 10
+// {
+//     testVariable = 20
+// }
+// console.log('VARIABLE - ', testVariable)
 
 
-const variable = '1234'
-variable = '567'
-console.log('let variable - ', variable);
+// /*--------------------CONST--------------------*/
+// /*-----First Example-----*/
+// testVariable
+// const testVariable = 10
+// console.log('VARIABLE - ', testVariable)
+
+// /*-----Second Example-----*/
+// testVariable = 10
+// const testVariable
+// console.log('VARIABLE - ', testVariable)
+
+// /*-----Third Example-----*/
+// testVariable = 10
+// console.log('VARIABLE - ', testVariable)
+// const testVariable
+
+// /*-----Fourth Example-----*/
+// testVariable = 10
+// {
+//     testVariable = 20
+// }
+// console.log('VARIABLE - ', testVariable)
+// const testVariable
+
+// /*-----Fifth Example-----*/
+// const testVariable = 10
+// {
+//     testVariable = 20
+// }
+// console.log('VARIABLE - ', testVariable)
+
+// /*-----Sixth Example-----*/
+// const testVariable = 10
+// console.log('VARIABLE - ', testVariable)
+
+
+// /*--------------------LET--------------------*/
+// /*-----First Example-----*/
+// testVariable
+// let testVariable = 10
+// console.log('VARIABLE - ', testVariable)
+
+// /*-----Second Example-----*/
+// testVariable = 10
+// let testVariable
+// console.log('VARIABLE - ', testVariable)
+
+// /*-----Third Example-----*/
+// testVariable = 10
+// console.log('VARIABLE - ', testVariable)
+// let testVariable
+
+// /*-----Fourth Example-----*/
+// testVariable = 10
+// {
+//     testVariable = 20
+// }
+// console.log('VARIABLE - ', testVariable)
+// let testVariable
+
+// /*-----Fifth Example-----*/
+// let testVariable = 10
+// {
+//     testVariable = 20
+// }
+// console.log('VARIABLE - ', testVariable)
+
+// /*-----Sixth Example-----*/
+// let testVariable = 10
+// console.log('VARIABLE - ', testVariable)
