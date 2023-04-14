29a2135cee5b75533d691d0cea2c016a7ad9bb0f
Vikas Saini <vikas.saini@spraxa.com>
2023-04-14 23:29:25 +0530

    vikas.saini_#Opertors

--- index.js
+++ index.js
@@ -8,3 +8,33 @@
 const variable = '1234'
 variable = '567'
 console.log('let variable - ', variable);
+
+
+//********************************* Operators ****************************//
+
+
+// Addition
+let a = [{a:1}];
+let b = [{b:3}];
+
+console.log("Sum -", a[0].a + b[0].b)
+
+// Subtract
+let c = [{a:1}];
+let d = [{b:3}];
+
+console.log("Sub -", a[0].a - b[0].b)
+
+// Multiply
+let e = [{a:2}];
+let f = [{b:3}];
+
+console.log("Mul -", e * f)
+
+// Equal 
+let g = "1"
+let h = 1
+
+console.log(g==h)
+console.log(g===h)
+
