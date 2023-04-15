bf927c964014b8a09cc002ce8979df84f6233dfb
pawan Chauhan <Pawanchauhan933@gmail.com>
2023-04-16 00:37:17 +0530

    1.Math Operators

--- index.js
+++ index.js
@@ -115,26 +115,125 @@
 
 
 // Addition
-let a = [{a:1}];
-let b = [{b:3}];
+// let a = [{a:1}];
+// let b = [{b:3}];
 
-console.log("Sum -", a[0].a + b[0].b)
+// console.log("Sum -", a[0].a + b[0].b)
 
-// Subtract
-let c = [{a:1}];
-let d = [{b:3}];
+// // Subtract
+// let c = [{a:1}];
+// let d = [{b:3}];
 
-console.log("Sub -", a[0].a - b[0].b)
+// console.log("Sub -", a[0].a - b[0].b)
 
-// Multiply
-let e = [{a:2}];
-let f = [{b:3}];
+// // Multiply
+// let e = [{a:2}];
+// let f = [{b:3}];
 
-console.log("Mul -", e * f)
+// console.log("Mul -", e * f)
 
-// Equal 
-let g = "1"
-let h = 1
+// // Equal
+// let g = "1"
+// let h = 1
 
-console.log(g==h)
-console.log(g===h)
+// console.log(g==h)
+// console.log(g===h)
+
+//Exponent Operators. : Raises a base number to the exponent power, that is, the base number multiplied by itself, exponent times.
+
+// console.log(5 ** 5);
+// console.log(5 * 5 * 5 * 5 * 5)
+// console.log(60 % 3);
+
+//+ ,- * % ()
+
+// console.log(2 + 2 - 4 * 8 + (7 + 7) % 2);
+// 4 - 32 + 14 % 2
+
+// 4 -32 + 0
+
+// console.log(50 + 10 / 8 + 2);
+
+// () % / * + -
+
+// Incermant Operators
+
+let num1 = 4;
+// console.log(num1++);
+// console.log(num1);
+// let num5 = 5;
+// console.log(num5--);
+// console.log(num5);
+// let num8 = 5;
+// console.log(num8--)
+// console.log(--num1);
+
+
+
+
+let x = 3; // x contains the value 3
+let y = 4; // y contains the value 4
+// console.log(x *= y);
+
+// console.log(x *= y);
+// console.log(x)
+// console.log(y);
+
+// console.log(x -= y);
+// console.log(x)
+// console.log(y);
+
+// console.log(x += y);
+// console.log(x)
+// console.log(y);
+
+// console.log(x /= y);
+
+// console.log(x)
+// console.log(y);
+
+// +++++++++++++++++++++++ Practice Question +++++++++++++++++++++
+
+// const a = 10, b = 14, c = 4, d = 2;
+
+// const e = a + b;
+
+// const f = c - d;
+
+// const finalResult = e * f;
+
+// console.log(finalResult)
+
+// if (finalResult % 2 == 0) {
+//     console.log(`this is even number`);
+// }
+
+
+// Question 2
+
+// let result = 5.21, result2 = 2;
+
+// const finalResult = result *= result2;
+// let finalNumber = finalResult;
+
+// console.log(finalNumber);
+
+
+// Question 3
+
+// weightComparison
+const tusharWeight = 90;
+const pawanWeight = 79;
+
+const weightComparisons = (tusharWeight > pawanWeight);
+console.log(weightComparisons);
+//heightComparison
+const tusharHeight = 6;
+const pawanHeight = 5.4;
+const heightComparison = (tusharHeight > pawanHeight);
+console.log(heightComparison);
+// pwdMatch
+const test1 = 'pawan@123';
+const test2 = 'Pawan@123';
+const pwdMatch = (test1 === test2);
+console.log(pwdMatch)
